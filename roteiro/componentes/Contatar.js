bf0ca5ca9b657aa901656ab32dbb1f325dcf8c8c
ValerioHasman <67921785+ValerioHasman@users.noreply.github.com>
2024-07-07 11:46:14 -0300

    Tirar telefonema

--- roteiro/componentes/Contatar.js
+++ roteiro/componentes/Contatar.js
@@ -45,12 +45,6 @@ export default class Contatar extends Component {
       </a>
     </div>
     <div class="col px-1">
-      <a class="rounded-4 bg-links text-body d-block" href="tel:+5512997329785" aria-label="Celular"
-        target="_blank">
-        <i class="bi  bi-telephone"></i>
-      </a>
-    </div>
-    <div class="col px-1">
       <a class="rounded-4 bg-links text-body d-block"
         href="https://wa.me/5512997329785?text=${Saudacao.apresentarEmURI()}" aria-label="WhatsApp" target="_blank">
         <i class="bi bi-whatsapp"></i>
